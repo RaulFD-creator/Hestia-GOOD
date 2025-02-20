eba9a904a79c1561dc8e66f77fbfb19b00a41f0a
Raúl Fernández Díaz <78709417+RaulFD-creator@users.noreply.github.com>
2025-02-20 11:33:38 +0000

    Update similarity.py

--- hestia/similarity.py
+++ hestia/similarity.py
@@ -242,8 +242,17 @@ def molecular_similarity(
         from rdkit.DataStructs import (
             BulkTanimotoSimilarity, BulkDiceSimilarity,
             BulkSokalSimilarity, BulkRogotGoldbergSimilarity,
-            BulkCosineSimilarity
-        )
+            BulkCosineSimilarity)
+        from rdkit import RDLogger
+        from rdkit import rdBase
+
+        def disable_rdkit_log():
+            """Disable all rdkit logs."""
+            for log_level in RDLogger._levels:
+                rdBase.DisableLog(log_level)
+
+        disable_rdkit_log()
+
     except ModuleNotFoundError:
         raise ImportError("This function requires RDKit to be installed.")
 
@@ -259,9 +268,13 @@ def molecular_similarity(
         fpgen = rdFingerprintGenerator.GetMorganGenerator(
             radius=radius, fpSize=bits
         )
-
         def _get_fp(smile: str):
-            mol = Chem.MolFromSmiles(smile)
+            mol = Chem.MolFromSmiles(smile, sanitize=True)
+
+            if mol is None:
+                print(f"SMILES: `{smile}` could not be processed. Will be substituted by `{smile[1:-1]}`")
+                return _get_fp(smile[1:-1])
+
             if sim_function in ['dice', 'tanimoto', 'sokal', 'rogot-goldberg',
                                 'cosine']:
                 fp = fpgen.GetFingerprint(mol)
